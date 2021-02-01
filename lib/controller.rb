baff8288cc247e7eb1d797679d104f014cf7608c
elio75006 <elioraph38@gmail.com>
2021-02-01 16:22:53 +0100

    added the find method

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,13 +1,19 @@
 require 'gossip'
 class ApplicationController < Sinatra::Base
   get '/' do
-   erb :index
+   erb :index, locals: {gossips: Gossip.all}
   end
   get '/gossips/new/' do
     erb :new_gossip
   end
   post '/gossips/new/' do
    Gossip.new(params["gossip_author"], params["gossip_content"]).save
-   puts params
-end
+   redirect '/'
+  end
+  
+  get '/gossips/:id' do
+    erb :show, locals: {id: params['id'].to_i, gossips: Gossip.find(params['id'].to_i)}
+  end
+
+  
 end
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -1,15 +1,32 @@
+
 class Gossip
-  attr_reader :author, :content
-  
-    def initialize(author, content)
+attr_reader :author, :content
+  def initialize(author, content)
     @author, @content = author, content
   end
+  
   def save
     CSV.open("./db/gossip.csv", 'a+') do |csv|
       csv << [@author, @content]
     end
   end
+  
   def self.all
-    CSV.read("./db/gossip.csv", headers: true) #,
+      all_gossips = []
+      
+    CSV.read("./db/gossip.csv").each do |csv_line|
+      all_gossips << Gossip.new(csv_line[0], csv_line[1])
+    end
+    return all_gossips
+  end
+  
+  def self.find(id)
+    selected_gossips = []
+    CSV.read("./db/gossip.csv").each_with_index do |csv_line, index| 
+      if (id == index+1)
+        selected_gossips << Gossip.new(csv_line[0], csv_line[1]) 
+      end
+    end 
+    return selected_gossips
   end
 end
